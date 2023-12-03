d30611688ff279f92bd9bc8b67b1b524cbec3390
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2023-12-03 21:43:14 +0500

    [init] project

---
+++ projectVIPER/projectVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projectVIPER
+//
+//  Created by Enzhe Gaysina on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
